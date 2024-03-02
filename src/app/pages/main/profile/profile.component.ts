b16f247e9d50b8198bab10fb5b79d43c77e27606
วีวี่ <161490194+PhutawanPTW@users.noreply.github.com>
2024-03-03 02:39:43 +0700

    new commit

--- src/app/pages/main/profile/profile.component.ts
+++ src/app/pages/main/profile/profile.component.ts
@@ -1,12 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ActivatedRoute } from '@angular/router';
+import { User } from '../../../model/model';
+import { ApiService } from '../../../services/api-service';
+import { ShareService } from '../../../services/share.service';
+import { Router } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    CommonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+  ],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+  styleUrl: './profile.component.scss',
 })
 export class ProfileComponent {
-  
+  url: string = 'sgshdh'
+  constructor(
+    private route: ActivatedRoute,
+    protected shareData: ShareService,
+    protected api: ApiService,
+    private router: Router
+  ) {}
+  navigateToMain() {
+    this.router.navigate(['/']);
+  }
+
+  navigateToSignUp() {
+    this.router.navigate(['/signup']);
+  }
+
+  navigateToLogin() {
+    localStorage.clear();
+    this.router.navigate(['/login']);
+    this.clearData();
+  }
+
+  clearData() {
+    this.shareData.userData = undefined;
+  }
+
+  navigateProfile() {
+    this.router.navigate(['/profile']);
+  }
+
+  navigateTop() {
+    this.router.navigate(['/top10']);
+  }
 }
